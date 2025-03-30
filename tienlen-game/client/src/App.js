7bb265975e9e4969528cf5170e7fe2259d6be277
kehuydiethoangdacap <kehuydiethoangdacap@gmail.com>
2025-03-30 08:17:18 +0700

    Add tienlen-game/client/src/App.js 1743297437065

---
+++ tienlen-game/client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Lobby from './pages/Lobby.jsx';
+
+function App() {
+  return (
+    <div className="app">
+      <Lobby />
+    </div>
+  );
+}
+
+export default App;
